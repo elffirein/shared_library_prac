a9425ba3c15942f82b1dca7d684ac557bd4139b6
EC2 Default User <ec2-user@ip-172-31-15-100.us-east-2.compute.internal>
2024-08-31 11:15:57 +0000

    called secondClass

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -4,6 +4,7 @@ import org.common.SecondClass
 def call(body) {
   def pipelineParams= [:]
   def example = new ExampleClass()
+  def second = new SecondClass()
   body.resolveStrategy = Closure.DELEGATE_FIRST
   body.delegate = pipelineParams
   body()
